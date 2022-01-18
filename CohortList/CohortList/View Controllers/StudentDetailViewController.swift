774e9be48b7c467d90821d6608b3a0e269ec3bdc
Karl Pfister <kpfister@me.com>
2022-01-18 16:55:17 -0700

    Completed project

--- CohortList/CohortList/View Controllers/StudentDetailViewController.swift
+++ CohortList/CohortList/View Controllers/StudentDetailViewController.swift
@@ -8,27 +8,58 @@
 import UIKit
 
 class StudentDetailViewController: UIViewController {
-
+    
     //MARK: - Outlets
     @IBOutlet weak var studentNameTextField: UITextField!
     @IBOutlet weak var cohortNumberTextField: UITextField!
     @IBOutlet weak var studentDescriptionTextView: UITextView!
     @IBOutlet weak var clearButton: UIButton!
-    
     @IBOutlet weak var deleteButton: UIButton!
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews(with: studentReciever)
+    }
+    //MARK: - Properties
+    var studentReciever: Student? = nil
+    
+    //MARK: - Helper Methods
+    func updateViews(with student: Student?) {
+        guard let student = student else { return }
+        studentNameTextField.text = student.name
+        cohortNumberTextField.text = String(student.cohortNumber)
+        studentDescriptionTextView.text = student.studentDescription
+    }
+    
+    func clearData() {
+        studentDescriptionTextView.text = ""
+        cohortNumberTextField.text = ""
+        studentNameTextField.text = ""
     }
     
     //MARK: - Actions
     @IBAction func clearButtonTapped(_ sender: Any) {
+        clearData()
     }
+    
     @IBAction func deleteButtonTapped(_ sender: Any) {
+        if let studentReciever = studentReciever {
+            StudentController.sharedInstance.deleteStudent(student: studentReciever)
+            self.navigationController?.popViewController(animated: true)
+        }
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let name = studentNameTextField.text, let cohortNum = cohortNumberTextField.text, let studentDescription = studentDescriptionTextView.text, let studentCohortNumber = Int(cohortNum) else {return}
+        
+        if let studentReciever = studentReciever {
+           // has a value. the user wants to update
+            StudentController.sharedInstance.updateStudent(student: studentReciever, newName: name, newCohortNum:studentCohortNumber, newDescription: studentDescription)
+        } else {
+            // does not have value. the user wants to do create
+            StudentController.sharedInstance.createStudent(name: name, cohortNumber: studentCohortNumber, studentDescription: studentDescription)
+        }
+        self.navigationController?.popViewController(animated: true)
     }
-    
 } // End of class
--- CohortList/CohortList/View Controllers/StudentListTableViewController.swift
+++ CohortList/CohortList/View Controllers/StudentListTableViewController.swift
@@ -8,23 +8,29 @@
 import UIKit
 
 class StudentListTableViewController: UITableViewController, UITextFieldDelegate{
-
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tableView.reloadData()
+    }
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return StudentController.sharedInstance.studentList.count // 5
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "studentCell", for: indexPath)
-
+        
         // Configure the cell...
-
+        
         let studentList = StudentController.sharedInstance.studentList
         let studentForIndexPath = studentList[indexPath.row] // 0 // 1
         // studentList[0]
@@ -35,62 +41,34 @@ class StudentListTableViewController: UITableViewController, UITextFieldDelegate
         // run:3 (s0,r2) - student.name == "Adam"
         // run:4 (s0,r3)
         // run:5 (s0,r4)
-  
+        
         cell.textLabel?.text = studentForIndexPath.name
         cell.textLabel?.text?.append(": \(studentList[indexPath.row].cohortNumber)")
-    
+        
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-//            let student = Student(name: StudentController.sharedInstance.studentList[indexPath.row].name,
-//                                  age: StudentController.sharedInstance.studentList[indexPath.row].cohortNumber,
-//                                  favoriteColor: StudentController.sharedInstance.studentList[indexPath.row].studentDescription)
-//
-//            StudentController.sharedInstance.deleteStudent(student: student)
+            let student = StudentController.sharedInstance.studentList[indexPath.row]
+            
+            StudentController.sharedInstance.deleteStudent(student: student)
             
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } 
+        }
     }
     
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "toDetailVC" {
+            if let destination = segue.destination as? StudentDetailViewController,
+               let indexPath = tableView.indexPathForSelectedRow {
+                let studentToPass = StudentController.sharedInstance.studentList[indexPath.row]
+                destination.studentReciever = studentToPass
+            }
+        }
     }
-    */
-
-}
+} // End of class
 
