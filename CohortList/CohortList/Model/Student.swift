f7fba8185233a17ee5ee0850bd5d608630ef886b
Matheus Oliveira <matheus.oliveira254@gmail.com>
2022-09-20 14:52:33 -0600

    creation of the model file and class

---
+++ CohortList/CohortList/Model/Student.swift
@@ -0,0 +1,18 @@
+//
+//  Student.swift
+//  CohortList
+//
+//  Created by Matheus Oliveira on 9/20/22.
+//
+
+import Foundation
+
+class Student {
+    let name: String
+    let cohortID: String
+    
+    init(name: String, cohortID: String) {
+        self.name = name
+        self.cohortID = cohortID
+    }
+}
